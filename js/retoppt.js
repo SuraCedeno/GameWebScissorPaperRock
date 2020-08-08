61348fc32648fb34ef4b19ad40f41d6ef0c4355a
jaivic <jaivic.villegas@inkafarmadigital.pe>
2020-08-08 14:59:03 -0500

    subiendo cambio

--- js/retoppt.js
+++ js/retoppt.js
@@ -1,34 +1,71 @@
-var piedra = "piedra";
-var papel = "papel";
-var tijera = "tijera";
+var piedra = "x";
+var papel = "p";
+var tijera = "t";
+var p1Puntaje=0;
+var pcPuntaje=0;
+var pcJugada="";
+function jugarPiedra(){
+    jugar(piedra)
+}
+function jugarPapel(){
+    jugar(piedra)
+}
+function jugarTijera(){
+    jugar(tijera)
+}
 
-var resultado = function(user, pc) {
+function jugar(){
+    var pcJugada=jugadaDelComputador() 
+    resultado=comparar(tijera,pcJugada);
+    console.log(resultado)
+    alert(resultado)
+    imprimirResultados()
+}
+
+ function comparar(user, pc) {
     if (user != pc) {
 
         if (user === papel && pc === piedra) {
-            console.log("Ganaste Papel le gana a piedra");
-        }
-        if (user === tijera && pc === papel) {
-            console.log("Ganaste Tijera gana a papel");
-        }
-        if (user === piedra && pc === tijera) {
-            console.log("Ganaste Piedra gana a tijera");
+            jugadorGano()
+            return "Ganaste (Papel le gana a piedra)";
+            
+        }else  if (user === tijera && pc === papel) {
+            jugadorGano()
+            return "Ganaste (Tijera gana a papel)";
+           
+        }else  if (user === piedra && pc === tijera) {
+            jugadorGano()
+            return "Ganaste (Piedra gana a tijera)";
+            
         } else {
-            console.log(" Perdiste. Te ganó la PC");
+            pcGano()
+            return "Perdiste. Te ganó la PC";
+          
         }
     } else if (user === pc)
-        console.log("Ha sido empate");
+        return"Ha sido empate";
 };
 
 function jugadaDelComputador() {
-    var result           = '';
-    var characters       = 'xpt';
+    var characters       = piedra+papel+tijera;
     var charactersLength = characters.length;
- result += characters.charAt(Math.floor(Math.random() * charactersLength));
-   if (result=="x"){return "piedra"}
-   if (result=="p"){return "papel"}
-   if (result=="t"){return "tijera"}
-    
+ return characters.charAt(Math.floor(Math.random() * charactersLength));
  }
- 
- document.getElementById("demo").innerHTML=jugadaDelComputador();
+function jugadorGano(){
+   p1Puntaje++;  
+}
+function pcGano(){
+   pcPuntaje++; 
+}
+
+ ///////////////////////////////////////// cosas que interactuan con el html
+ function imprimirResultados(){
+    imprimirJugadorPuntaje();
+    imprimirPcPuntaje();
+ }
+ function imprimirJugadorPuntaje(){
+    document.getElementById("p1puntaje").innerHTML=p1Puntaje;  
+}
+function imprimirPcPuntaje(){
+    document.getElementById("pcpuntaje").innerHTML=pcPuntaje; 
+}
